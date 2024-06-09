e9d07d0dc5097eb54c94fd4766da585f4ca41ee6
sagzone <spkawad21@gmail.com>
2024-06-09 09:14:45 +0530

    basic mongodb operation setted up

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://spkawad21:od4j59PaMgPl7tmi@cluster0.fwvfyuv.mongodb.net/bluebikes?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected!"));
+
+const Product = mongoose.model("Products", { name: String });
+
+const product = new Product({
+  name: "sam",
+});
+
+product.save().then(() => {
+  console.log("product successfully stored!");
+});
 
 app.listen(3000, () => {
   console.log("server is up and running on port 3000");
